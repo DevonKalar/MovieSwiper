ef706f8e51bf3dec9ee8a88c671388f89d716c39
DevonKalar <devonkalar@gmail.com>
2025-08-28 10:57:41 -0700

    add swiper button handling

--- src/components/features/SwipeCard.jsx
+++ src/components/features/SwipeCard.jsx
@@ -1,32 +1,49 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import { fetchMovieDetails } from "../../services/movieService.js";
 
 const SwipeCard = () => {
-  const [movieId, setMovieId] = useState(273);
-  const [movieData, setMovieData] = useState({
-    title: "",
-    description: "",
-    poster: "",
-  });
 
+  const getMovieId = () => {
+    const newMovieId = Math.floor(Math.random() * 1000);
+    console.log("Generated new movie ID:", newMovieId);
+    return newMovieId;
+  };
+
+  const [movieId, setMovieId] = useState(getMovieId);
+  const [movieData, setMovieData] = useState({});
+
+  const getMovieData = async (id) => {
+    const data = await fetchMovieDetails(id);
+    console.log("Fetched movie data:", data);
+    if (data) {
+      setMovieData(data);
+    }
+  };
 
   useEffect(() => {
-    console.log("useEffect Ran!");
-    const fetchData = async () => {
-      if (movieId) {
-        const data = await fetchMovieDetails(movieId);
-        console.log("Fetched movie data:", data);
-        if (data) {
-          setMovieData(data);
-        }
-      }
-    };
-    fetchData();
+    getMovieData(movieId);
   }, [movieId]);
 
+  const handleSubmit = (e) => {
+    const buttonValue = e.currentTarget.value
+    switch (buttonValue) {
+      case 'yes':
+    console.log("User liked the movie:", movieData.title);
+    break;
+      case 'no':
+    console.log("User disliked the movie:", movieData.title);
+    break;
+      case 'pass':
+    console.log("User passed on the movie:", movieData.title);
+    break;
+      default:
+    console.log("Unknown button value:", buttonValue);
+    }
+    setMovieId(getMovieId());
+  }
+
   return (
-    <>
+    <div className="swipe-card-box">
       <div className="swipe-card">
         <div className="image-container">
           <img src={movieData.poster} alt="Movie Poster" />
@@ -35,16 +52,13 @@ const SwipeCard = () => {
             <p>{movieData.description}</p>
           </div>
         </div>
-        <div>
-            <p>more info</p>
-        </div>
       </div>
       <div className="AnswerWrapper">
-        <button className="swipe-button">Nope</button>
-        <button className="swipe-button">Pass</button>
-        <button className="swipe-button">Yeah</button>
+        <button onClick={handleSubmit} className="swipe-button" value="no">Nope</button>
+        <button onClick={handleSubmit} className="swipe-button" value="pass">Pass</button>
+        <button onClick={handleSubmit} className="swipe-button" value="yes">Yeah</button>
       </div>
-    </>
+    </div>
   );
 };
 
