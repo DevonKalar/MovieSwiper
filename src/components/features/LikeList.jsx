1aa399c604f0407d35b3d1fe24417f0a4a9ee819
DevonKalar <devonkalar@gmail.com>
2025-08-28 14:02:16 -0700

    add boilerplate jsx to LikeList

--- src/components/features/LikeList.jsx
+++ src/components/features/LikeList.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react';
-
 const LikeList = () => {
 
     return (
-        <>
-            <h2>Liked Movies</h2>
-        </>
+        <div className="like-list-box">
+            <h1>Liked Movies</h1>
+            <div className="like-list">
+                { /* in the future we will iterate over liked movies here, and have a condition for no liked movies */ }
+                <div className="like-item">
+                    <img src="https://dummyimage.com/800x1000/000/fff" alt="Movie Poster" />
+                    <div className="like-item-info">
+                        <h2>Movie Title</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 
 }
